4e6b82a265d75d1b5634496eeecb910723eaf2ce
Carlos Rafael <carlosrafael093@gmail.com>
2016-06-12 23:10:14 -0300

    Ajeitando o find_features, agora ele retorna {} dizendo quais words tem e nao tem no document

--- textClassification.py
+++ textClassification.py
@@ -9,59 +9,60 @@ for category in movie_reviews.categories():
 	for fileid in movie_reviews.fileids(category):
 		documents.append((movie_reviews.words(fileid), category))
 
-print(documents[0])
-print('\n')
+# print(documents[0])
+# print('\n')
+
 # Vai ver quais sao as words mais frequentes em todos os documentos
-all_words = nltk.FreqDist(movie_reviews.words())
-print(all_words.most_common(20))
-print('\n')
+all_words = nltk.FreqDist(movie_reviews.words()) #retorna tuplas (words, frequency)
 
 # Fazendo o stopwords nas palavras dos documentos pra tirar muita coisa inutil
 stop_words = set(stopwords.words("english"))
 
 all_words_no_stopwords = [w for w in movie_reviews.words() if w not in stop_words]
 all_words_no_stopwords = nltk.FreqDist(all_words_no_stopwords)
-print(all_words_no_stopwords.most_common(20))
+#print(all_words_no_stopwords.most_common(20))
 
 
 #Vou tentar melhorar a lista de stopwords colocando nela algumas pontuacoes q nao servem de nada
 # Fiz elas com unicode pq eh assim que as stop_words estao
-punctuation = [u'.', u'-', u',', u'"', u'(', u')', u':', u'?', u"'"]
+punctuation = [u'.', u'-', u',', u'"', u'(', u')', u':', u'?', u"'", u'--', u';', u'!', u'$']
 punctuation = set(punctuation)
-print(punctuation)
 new_stop_words = stop_words.union(punctuation)
-print(new_stop_words)
-print('\n')
 
+#Pegando todas as palavras do movie_reviews sem as stopwords
 all_words_no_stopwords = [w for w in movie_reviews.words() if w not in new_stop_words]
 all_words_no_stopwords = nltk.FreqDist(all_words_no_stopwords)
 print(all_words_no_stopwords.most_common(20))
 
 print(len(all_words_no_stopwords)) #Tem 39608 palavras
-print(all_words_no_stopwords.most_common()[25000]) #Pega a palavra 25000
+#print(all_words_no_stopwords.most_common()[25000]) #Pega a palavra 25000
 #Pra saber o numero de vezes que certa palavra apareceu
 print(all_words_no_stopwords["stupid"])
 
 #Pega as primeiras 25000 palavras da tupla pra servir como os features
 # que vao ser usadas como parametros pra avaliar positivo ou negativo
-word_features = list(all_words_no_stopwords.keys())[:25000]
+#word_features = list(all_words_no_stopwords.keys())[:50]
 
-print(word_features[:100])
-print('\n')
+#Pega as 20.000 palavras mais comuns de todos os reviews para servirem de features ao avaliarmos novos reviews
+top_word_features = all_words_no_stopwords.most_common(20000) #retorna (u'revolutionaries', 3)
+
+#Como top_wf retorna (word,freq) iremos pegar so as palavras que sao as keys
+top_word_features_keys = [wf[0] for wf in top_word_features]
+print(top_word_features_keys[550:575])
+
 #Retorna uma lista com True ou False dizendo quais palavras da word_features o documento tem
+# retorna: {u'even': True, u'story': False, u'also': True, u'see': True, u'much': False,.... }
 def find_features(document):
 	# Pega todas as palavras do documento e transforma em set pra retornar as palavras independente da frequencia dela
 	words = set(document)
 	features = {}
-	#temaqui = []
-	for w in word_features:
+	counter = 0
+	print(top_word_features_keys[:20])
+	for w in top_word_features_keys[:20]:
 		features[w] = (w in words)
-		# if(w in words):
-		# 	temaqui.append(w)
 
 	return features
 
-#print(movie_reviews.words('neg/cv000_29416.txt'))
 print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
 
 #featureSet = [(find_features(rev), category) for (rev, category) in documents]
